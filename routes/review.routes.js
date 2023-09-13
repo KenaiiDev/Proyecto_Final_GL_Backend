c3b5596427d449a28eeac434522b5af6055650b9
Bixio <bbbissio.a@gmail.com>
2023-09-13 20:27:09 -0300

    Review Routes & Get Review by Movie ID

---
+++ routes/review.routes.js
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+import { reviewController } from '../controllers/review.controller.js'
+
+export const reviewRoutes = () => {
+    const reviewRouter = Router()
+    const { createReview, deleteReview, getReviews, getReviewById, updateReview, getReviewsByMovieId } = reviewController()
+
+    reviewRouter.route('/reviews').get(getReviews).post(createReview)
+
+    reviewRouter.route('/reviews/:id').get(getReviewById).put(updateReview).delete(deleteReview)
+
+    reviewRouter.route('/reviews/movie/:id').get(getReviewsByMovieId)
+}
