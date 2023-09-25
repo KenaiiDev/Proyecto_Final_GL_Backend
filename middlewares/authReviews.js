a649385ef83a92c8dae3e5eed4a40927f39fc661
Lucas Villanueva <lucasvillanuevagmp@gmail.com>
2023-09-24 23:46:42 -0300

    Fix a syntaxis error in authReviews

--- middlewares/authReviews.js
+++ middlewares/authReviews.js
@@ -34,7 +34,7 @@ export const authReviews = async (req, res, next) => {
 };
 
 export const authCreateReview = async (req, res, next) => {
-  constconst headers = req.headers;
+  const headers = req.headers;
   const authorization = headers && headers.authorization;
   const token = authorization && authorization.split(" ")[1];
 
