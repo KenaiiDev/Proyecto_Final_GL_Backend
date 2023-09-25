be93540724ce6088dc6444fc9830cffbe634a3de
Lucas Villanueva <lucasvillanuevagmp@gmail.com>
2023-09-24 23:43:41 -0300

    "Fix an error with authReviews middleware"

--- middlewares/authReviews.js
+++ middlewares/authReviews.js
@@ -34,9 +34,16 @@ export const authReviews = async (req, res, next) => {
 };
 
 export const authCreateReview = async (req, res, next) => {
-  const headers = req.headers;
-  const { authorization } = headers;
-  const token = authorization.split(" ")[1];
+  constconst headers = req.headers;
+  const authorization = headers && headers.authorization;
+  const token = authorization && authorization.split(" ")[1];
+
+  if (!token) {
+    return res.status(HTTP_STATUS.UNAUTHORIZED).json({
+      success: false,
+      message: "Unauthorized User",
+    });
+  }
 
   const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
   const { id } = decodedToken.user;
